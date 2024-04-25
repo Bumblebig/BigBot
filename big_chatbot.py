1cd9897ba976ae3d75082aa58bcad160df90a3a1
Abdurrahman <dreydare16@gmail.com>
2024-04-25 14:49:01 +0100

    Initialised gemini AI as backtrack"

--- big_chatbot.py
+++ big_chatbot.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import openai
+import textwrap
+import re
+import google.generativeai as genai
+from IPython.display import Markdown
 
-# OpenAI API key
-openai.api_key = "key"
 
 # read data from Excel
 data = pd.read_excel("BigBotData.xlsx")
@@ -14,17 +15,28 @@ for idx, row in data.iterrows():
     bot_response = row['bot_response']
     responses[user_input] = bot_response
 
-# generate bot response using GPT-3
+# Initializing Gemini
+genai.configure(api_key="API_KEY")
+model = genai.GenerativeModel('gemini-pro')
 
+# Format bot response
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+# generate bot response using Gemini
 def generate_bot_response(user_input):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": user_input}]
-    )
-    return response.choices[0].message.content.strip()
+    response = model.generate_content(user_input)
+    return to_markdown(response.text)
+
+# Remove unwanted charcters
+def format_text(text):
+    pattern = r'[>*]'
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text
 
 # Initialize the chat bot
-print("BigBot: Hi! I'm your chatbot")
+print("BigBot: Hi! I'm your chatbot. Type 'exit' to quit")
 while True:
     user_input = input("You: ").lower()
     
@@ -36,5 +48,4 @@ while True:
         print("BigBot:", responses[user_input])
     else:
         bot_response = generate_bot_response(user_input)
-        print("BigBot:", bot_response)
-
+        print(f"BigBot:{format_text(bot_response.data)}")
